18c36b32200a97708d490e331c49337bd797d878
NaN-App <renanmoreira89@gmail.com>
2026-07-17 06:17:46 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker para BellaZo - Suporte Offline e Sincronização
-const CACHE_NAME = 'bellazo-v2.2';
+const CACHE_NAME = 'bellazo-v3.2';
 const urlsToCache = [
   './',
   'index.html',
